6905876e631aa366e9e7a2556013fd62c0611b21
seinosuke14 <diegobascur9@gmail.com>
2026-01-31 19:16:56 -0300

    dart

---
+++ 01_hellow_world.dart
@@ -0,0 +1,5 @@
+void main() {
+  const myName = "Diego";
+
+  print('Hola Mundo este es mi Nombre: $myName');
+}
---
+++ 03_maps.dart
@@ -0,0 +1,30 @@
+void main() {
+  final Map<String, dynamic> pokemon = {
+    'name': 'Ditto',
+    'hp': 100,
+    'isAlive': true,
+    'abilities': <String>['impostor'],
+    'sprites': {
+      1: 'ditto/front.png',
+      2: 'ditto/back.png',
+      3: {
+        'dentro': 'ditto/front_shiny.png',
+        'fuera': 'ditto/back_shiny.png',
+      }
+    },
+  };
+
+  final Map<int, dynamic> pokemons = {
+    1: 'Bulbasaur',
+    2: 'Ivysaur',
+    3: 'Venusaur',
+    4: true
+  };
+
+  print(pokemon['sprites']);
+
+  print(pokemon['sprites'][1]);
+  print(pokemon['sprites'][2]);
+  print(pokemon['sprites'][3]);
+  print(pokemon['sprites'][3]['dentro']);
+}
